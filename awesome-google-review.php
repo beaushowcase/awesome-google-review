c507a673848d3c18aa4142b6e04a099221b13174
beaushowcase <beaudigital2024@gmail.com>
2024-05-20 14:28:04 +0530

    update 1.2
    update 1.2

--- awesome-google-review.php
+++ awesome-google-review.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Awesome Google Review
  * Plugin URI:        https://beardog.digital/
  * Description:       Impresses with top-notch service and skilled professionals. A 5-star destination for grooming excellence!
- * Version:           1.1
+ * Version:           1.2
  * Requires PHP:      7.2
  * Author:            #beaubhavik
  * Author URI:        https://beardog.digital/
